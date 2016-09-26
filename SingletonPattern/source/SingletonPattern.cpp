8bfe77fbcdf72a030a6cf3b4d73033ac0b340401
2919108204@qq.com <2919108204@qq.com>
2016-09-26 15:14:56 +0800

    单例模式

---
+++ SingletonPattern/source/SingletonPattern.cpp
@@ -0,0 +1,13 @@
+// SingletonPattern.cpp : 定义控制台应用程序的入口点。
+//
+
+#include "stdafx.h"
+#include <stdlib.h>
+
+int _tmain(int argc, _TCHAR* argv[])
+{
+	printf("123\n");
+	system("PAUSE");
+	return 0;
+}
+
